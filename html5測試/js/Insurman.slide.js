976492610816e0c4ae980f4619bf6e4d35a25e8a
BEN <benson52863@gmail.com>
2018-08-28 22:53:00 +0800

    修改網站

--- html5測試/js/Insurman.slide.js
+++ html5測試/js/Insurman.slide.js
@@ -8,48 +8,49 @@ $(document).ready(function() {
         $('html, body').animate({scrollTop:'0%' }, 800);
     });
 });
-$(document).ready(function() {
-    $("#menu1").click(function() {
-       $('html, body').animate({scrollTop:'617vh' }, 800);
-    });
+$(document).ready(function(){
+    $(function(){ $('#menu1').click(function(){ 
+        $('html,body').animate({scrollTop:$('#hr01').offset().top}, 800);});  
+    }); 
+}); 
+$(document).ready(function(){
+    $(function(){ $('#menu2').click(function(){ 
+        $('html,body').animate({scrollTop:$('#hr02').offset().top}, 800);});  
+    }); 
+}); 
+$(document).ready(function(){
+    $(function(){ $('#menu3').click(function(){ 
+        $('html,body').animate({scrollTop:$('#hr03').offset().top}, 800);});  
+    }); 
+}); 
+$(document).ready(function(){
+    $(function(){ $('#action-1').click(function(){ 
+        $('html,body').animate({scrollTop:$('#hr03').offset().top}, 800);});  
+    }); 
 });
-$(document).ready(function() {
-    $("#menu2").click(function() {
-        $('html, body').animate({scrollTop:'1230vh' }, 800);
-    });
+$(document).ready(function(){
+    $(function(){ $('#action-2').click(function(){ 
+        $('html,body').animate({scrollTop:$('#hr03').offset().top}, 800);});  
+    }); 
 });
-$(document).ready(function() {
-    $("#menu3").click(function() {
-        $('html, body').animate({scrollTop:'1850vh' }, 800);
-    });
+$(document).ready(function(){
+    $(function(){ $('#action-3').click(function(){ 
+        $('html,body').animate({scrollTop:$('#hr03').offset().top}, 800);});  
+    }); 
 });
-$(document).ready(function() {
-    $("#action-1").click(function() {
-        $('html, body').animate({scrollTop:'1850vh' }, 800);
-    });
+$(document).ready(function(){
+    $(function(){ $('#menu4').click(function(){ 
+        $('html,body').animate({scrollTop:$('#hr04').offset().top}, 800);});  
+    }); 
 });
-$(document).ready(function() {
-    $("#action-2").click(function() {
-        $('html, body').animate({scrollTop:'1850vh' }, 800);
-    });
-});
-$(document).ready(function() {
-    $("#action-3").click(function() {
-        $('html, body').animate({scrollTop:'1850vh' }, 800);
-    });
-});
-$(document).ready(function() {
-    $("#action-4").click(function() {
-        $('html, body').animate({scrollTop:'1850vh' }, 800);
-    });
-});
-$(document).ready(function() {
-    $("#action-5").click(function() {
-        $('html, body').animate({scrollTop:'1850vh' }, 800);
-    });
-});
-$(document).ready(function() {
-    $("#menu4").click(function() {
-        $('html, body').animate({scrollTop:'2500vh' }, 800);
-    });
-});
+$(window).load(function(){
+            $(".col-3 input").val("");
+            
+            $(".input-effect input").focusout(function(){
+                if($(this).val() != ""){
+                    $(this).addClass("has-content");
+                }else{
+                    $(this).removeClass("has-content");
+                }
+            })
+        });
