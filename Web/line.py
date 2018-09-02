9a0b6f0f2f345a9a5a947e1173d3aa058d0dec9d
BEN <benson52863@gmail.com>
2018-09-02 23:41:01 +0800

    測試網頁

--- Web/line.py
+++ Web/line.py
@@ -7,6 +7,7 @@ from getData.golden import getGolden
 from getData.Currency import getCurrency
 from getData.data import getSite,getStock,getStocNum
 
+
 import re
 from linebot import (
     LineBotApi, WebhookHandler
@@ -41,6 +42,7 @@ def send_statics(path):
 
 @application.route('/_add_numbers')
 def add_numbers():
+
     query = request.args.get('query')
     query = query.upper()
     query = re.sub('臺','台',query)
@@ -85,7 +87,43 @@ def add_numbers():
             regex = re.compile('(美金|日圓|日元|人民幣|英鎊|歐元)')
             query = regex.search(query)
             reply = getCurrency(query.group(1))
-            
+
+
+        #請問本公司之續次保費繳費方式有哪些？
+        elif(re.findall('續', query)!=None):
+            if (re.findall('保費|保險費', query)!=None):
+                if (re.findall('繳', query)!=None):
+                    reply = '本公司之續次保費繳費方式：人員收費、金融機構轉帳、信用卡代繳、保戶自行繳費(如郵撥..)'
+
+        #我收到繳費通知，但沒有人來收保費應該怎麼辦？
+        elif(re.findall('沒有', query)!=None):
+            if (re.findall('收', query)!=None):
+                if (re.findall('保費', query)!=None):
+                    reply = '1. 您可以依繳費通知上記載之收費單位地址、電話逕與收費人員連絡。 2. 您可以撥打保戶服務專線0809-000-550與本公司連絡，來電時請留下保單號碼、電話及地址，以便通知收費人員與您連絡。'
+        
+        #請問支票抬頭應如何開立？
+        elif(re.findall('支票', query)!=None):
+            if (re.findall('抬頭', query)!=None):
+                reply = '本公司之續次保費繳費方式：人員收費、金融機構轉帳、信用卡代繳、保戶自行繳費(如郵撥..)'
+
+        #我若移居國外，其收費情形應如何處理？
+        elif(re.findall('國外', query)!=None):
+            if (re.findall('收費', query)!=None):
+                reply = '本公司在國外並未設立營業據點，無法受理保戶將收費地址變更至國外，所以保戶移居國外，仍須於國內留有收費地址，可委託國內之親友代繳或以自動轉帳、信用卡方式扣繳保費（仍須於國內設有轉帳帳戶）。'
+
+        #我遺失送金單時，該怎麼辦？
+        elif(re.findall('遺失|不見|補發', query)!=None):
+            if (re.findall('送金單', query)!=None):
+                if (re.findall('開立|填寫', query)!=None):
+                    reply = '1.如果可以確定已繳費入帳，則送金單遺失並不影響您的保險權益。 2.如您不放心，可向保費帳務部或分公司、地區行政中心申請補發，惟補發後您不得持原送金單向本公司作任何主張。'
+
+        #我的保單服務員是誰？
+        elif(re.findall('保單', query)!=None):
+            if (re.findall('服務員', query)!=None):
+                reply = '富邦金控官網中的「人壽保戶會員專區」有提供保單相關資料的查詢。您必須是富邦保戶並申請加入會員，即可透過網路查詢到以您為要保人的相關保單資料。'
+
+        
+
         else:
             query = query.upper()
             response = runAIML(query)
