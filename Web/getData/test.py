2db0af5e8f3cae4e9dd7c9282665552b96f76f46
jacky860328 <jacky860328@gmail.com>
2018-09-05 16:16:10 +0800

    erogjwpighqeruogh3

--- Web/getData/test.py
+++
@@ -1,9 +0,0 @@
-from runAIML import runAIML
-
-inpt = '你好'
-test = runAIML(inpt)
-if test == '':
-    print('QQ')
-else:
-    print('QQQ')
-print(test)
--- Web/line.py
+++ Web/line.py
@@ -1,11 +1,5 @@
 from flask import Flask, send_file, send_from_directory, request, jsonify, render_template, abort
-from getData.weather import getWeather
 from getData.runAIML import runAIML
-from getData.stock2 import getfixstock
-from getData.oilPrice import getOilPrice
-from getData.golden import getGolden
-from getData.Currency import getCurrency
-from getData.data import getSite,getStock,getStocNum
 
 import re
 from linebot import (
@@ -47,47 +41,10 @@ def add_numbers():
     reply = '我不知道你在說什麼'
     
     try:
-        if(re.search('氣溫',query)!=None):
-            regex = re.compile(getSite())
-            query = regex.search(query)
-            reply=getWeather(query.group(1),0)
-        elif(re.search('天氣',query)!=None):
-            regex = re.compile(getSite())
-            query = regex.search(query)
-            reply=getWeather(query.group(1),2)
-        elif(re.search(getStock(),query)!=None):
-            stock = getStocNum()
-            regex = re.compile(getStock())
-            stockname = regex.search(query)
-            if (re.search('(市|買|賣|成交|收|開|高|低)',query)!=None):
-                regex = re.compile('(市|買|賣|成交|收|開|高|低)')
-                select ={'市':1,'買':2,'賣':3,'成交':4,'收':5,'開':6,'高':7,'低':8} 
-                stockCode =regex.search(query)
-                reply =  getfixstock(stock[stockname.group(0)],select[stockCode.group(0)])
-            else:
-                reply = getfixstock(stock[stockname.group(0)],0)
-        elif(re.search('\d',query)!=None):
-            regex = re.compile('\d+')
-            stocknum=regex.search(query)
-            if (re.search('(市|買|賣|成交|收|開|高|低)',query)!=None):
-                regex = re.compile('(市|買|賣|成交|收|開|高|低)')
-                select ={'市':1,'買':2,'賣':3,'成交':4,'收':5,'開':6,'高':7,'低':8} 
-                stockCode =regex.search(query)
-                reply =  getfixstock(stocknum.group(),select[stockCode.group(0)])
-            else:
-                reply = getfixstock(stocknum.group(),0)
-        elif(re.search('油價',query)!=None):
-            reply = getOilPrice()
-    
-        elif(re.search('黃金',query)!=None):
-            reply = getGolden()
-        elif(re.search('(美金|日圓|日元|人民幣|英鎊|歐元)',query)!=None):
-            regex = re.compile('(美金|日圓|日元|人民幣|英鎊|歐元)')
-            query = regex.search(query)
-            reply = getCurrency(query.group(1))
+        
 
         #請問本公司之續次保費繳費方式有哪些？
-        elif(re.search('續', query)!=None):
+        if(re.search('續', query)!=None):
             if (re.search('保費|保險費', query)!=None):
                 if (re.search('繳', query)!=None):
                     reply = '本公司之續次保費繳費方式：人員收費、金融機構轉帳、信用卡代繳、保戶自行繳費(如郵撥..)'
@@ -207,76 +164,7 @@ def callback():
         abort(400)
 
     return 'ok'
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    reply = "我不知道你在說什麼"
-    query = event.message.text
-    query = query.upper()
-    query = re.sub('臺','台',query)
-    
-    if(re.search('氣溫',query)!=None):
-        regex = re.compile('(台北|新北|台中|高雄|台南|桃園|基隆|新竹|雲林|南投|嘉義|苗栗|彰化|花蓮|台東|澎湖|宜蘭)')
-        query = regex.search(query)
-        reply=getWeather(query.group(1),0)
-        line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=reply))
-    elif(re.search('天氣',query)!=None):
-        regex = re.compile('(台北|新北|台中|高雄|台南|桃園|基隆|新竹|雲林|南投|嘉義|苗栗|彰化|花蓮|台東|澎湖|宜蘭)')
-        query = regex.search(query)
-        reply=getWeather(query.group(1),2)
-        line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=reply))
-    elif(re.search(getStock(),query)!=None):
-        stock = getStocNum()
-        regex = re.compile(getStock())
-        stockname = regex.search(query)
-        if (re.search('(市|買|賣|成交|收|開|高|低)',query)!=None):
-            regex = re.compile('(市|買|賣|成交|收|開|高|低)')
-            select ={'市':1,'買':2,'賣':3,'成交':4,'收':5,'開':6,'高':7,'低':8} 
-            stockCode =regex.search(query)
-            reply =  getfixstock(stock[stockname.group(0)],select[stockCode.group(0)])
-        else:
-            reply = getfixstock(stock[stockname.group(0)],0)
-      
-        line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=reply))
-    elif(re.search('油價',query)!=None):
-        reply = getOilPrice()
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=reply))
-    
-    elif(re.search('黃金',query)!=None):
-        line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=getGolden()))
-    elif(re.search('(美金|日圓|日元|人民幣|英鎊|歐元)',query)!=None):
-        regex = re.compile('(美金|日圓|日元|人民幣|英鎊|歐元)')
-        query = regex.search(query)
-        reply = getCurrency(query.group(1))
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=reply))
-    elif(re.search('\d',query)!=None):
-        regex = re.compile('\d+')
-        stocknum=regex.search(query)
-        if (re.search('(市|買|賣|成交|收|開|高|低)',query)!=None):
-            regex = re.compile('(市|買|賣|成交|收|開|高|低)')
-            select ={'市':1,'買':2,'賣':3,'成交':4,'收':5,'開':6,'高':7,'低':8} 
-            stockCode =regex.search(query)
-            reply =  getfixstock(stocknum.group(),select[stockCode.group(0)])
-        else:
-            reply = getfixstock(stocknum.group(),0)
-        line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=reply))
-    else:
-        query = query.upper()
-        response = runAIML(query)
-        if response != '':
-            reply = response
-            line_bot_api.reply_message(event.reply_token,TextSendMessage(text=reply))
-        else:
-            line_bot_api.reply_message(event.reply_token,TextSendMessage(text=reply))
+
 
           
 if __name__ == "__main__":
